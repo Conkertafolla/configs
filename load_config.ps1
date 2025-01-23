3b246aaebb6caeb0f44e466b842298a5a91a74b3
Conker <conkertafolla2.0@gmail.com>
2025-01-23 00:16:56 -0600

    Modificacion para poder instalar nvim

--- load_config.ps1
+++ load_config.ps1
@@ -2,21 +2,21 @@ $baseConfigPath = $PSScriptRoot
 $nushellConfigPath = "$baseConfigPath\nushell\config.nu"
 $alacrittyConfigPath = "$baseConfigPath\alacritty\alacritty.toml"
 $starshipConfigPath = "$baseConfigPath\starship.toml"
-$lazyvimConfigPath = "$baseConfigPath\lazyvim"
+$lazyvimConfigPath = "$baseConfigPath\nvim"
 
 # Destinos de los enlaces simbólicos
 $nushellTargetPath = "$env:APPDATA\nushell\config.nu"
 $alacrittyTargetPath = "$env:APPDATA\alacritty\alacritty.yml"
 $starshipTargetPath = "$env:USERPROFILE\.config\starship.toml"
 
-#$lazyvimTargetPath = "$env:USERPROFILE\AppData\Local\nvim"
+$lazyvimTargetPath = "$env:USERPROFILE\AppData\Local\nvim"
 
 # Crear directorios necesarios
 Write-Host "Creando directorios necesarios..."
 New-Item -ItemType Directory -Force -Path "$env:APPDATA\nushell" | Out-Null
 New-Item -ItemType Directory -Force -Path "$env:APPDATA\alacritty" | Out-Null
 New-Item -ItemType Directory -Force -Path "$env:USERPROFILE\.config" | Out-Null
-#New-Item -ItemType Directory -Force -Path "$env:USERPROFILE\AppData\Local\nvim" | Out-Null
+New-Item -ItemType Directory -Force -Path "$env:USERPROFILE\AppData\Local\nvim" | Out-Null
 
 # Crear enlaces simbólicos
 Write-Host "Creando enlaces simbólicos..."
@@ -36,9 +36,9 @@ if (Test-Path $starshipTargetPath) {
 }
 cmd /c mklink $starshipTargetPath $starshipConfigPath
 
-#if (Test-Path $lazyvimTargetPath) {
-#   Remove-Item $lazyvimTargetPath -Recurse -Force
-#}
-#cmd /c mklink /D $lazyvimTargetPath $lazyvimConfigPath
+if (Test-Path $lazyvimTargetPath) {
+   Remove-Item $lazyvimTargetPath -Recurse -Force
+}
+cmd /c mklink /D $lazyvimTargetPath $lazyvimConfigPath
 
 Write-Host "Enlaces simbólicos creados exitosamente."
